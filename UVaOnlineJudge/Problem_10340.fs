cd6405e3ef1508c2cb465678a32bbd86fd4bcb04
Carlos Rodriguez <crodveg@yahoo.es>
2014-06-21 20:05:31 +0200

    Added problem 10340.

---
+++ UVaOnlineJudge/Problem_10340.fs
@@ -0,0 +1,20 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_10340 =
+  // Input
+  let s = "VERDI"
+  let t = "vivaVittorioEmanueleReDiItalia"
+
+  let rec isSubsequence s t =
+    match s with
+    | [] -> Some true
+    | head :: tail -> 
+      match List.tryFindIndex (fun x -> x = head) t with
+      | Some value -> 
+        let arr = Array.ofList t
+        isSubsequence tail [for i in value .. (arr.Length - 1) do yield arr.[i]]
+      | None -> None
+
+  match isSubsequence (Array.toList (s.ToCharArray())) (Array.toList (t.ToCharArray())) with
+  | Some value -> printfn "Yes"
+  | None -> printfn "No"
