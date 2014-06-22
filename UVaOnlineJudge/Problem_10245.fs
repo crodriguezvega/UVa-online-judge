5cedd28dba75c67948d82304d9878e1d153ef057
Carlos Rodriguez <crodveg@yahoo.es>
2014-06-22 13:07:28 +0200

    Added problem 10245.

---
+++ UVaOnlineJudge/Problem_10245.fs
@@ -0,0 +1,28 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_10245 =
+  type Coordinate2D (x, y) = class
+    member this.x = x
+    member this.y = y
+    member this.Distance (point: Coordinate2D) = sqrt (float ((pown (x - point.x) 2) + (pown (y - point.y) 2)))
+  end
+
+  let printDistance (distance: float) =
+    match distance with
+    | _ when distance >= 10000.0 -> printfn "INFINITY"
+    | _ -> printfn "%F" distance
+
+  let distanceBetweenPoints points =
+    let rec loop (points: Coordinate2D list) distances =
+      match points with
+      | [] -> distances
+      | head :: tail -> loop tail ((List.map (fun x -> head.Distance x) tail) @ distances)
+    loop points List.Empty
+
+  [new Coordinate2D (0, 2);
+   new Coordinate2D (6, 67);
+   new Coordinate2D (43, 71);
+   new Coordinate2D (39, 107);
+   new Coordinate2D (189, 140)] |> distanceBetweenPoints
+                                |> List.min
+                                |> printDistance
