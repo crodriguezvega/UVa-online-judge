630f78b6112c8b4720a2909811bc0f03aa7c3455
Carlos Rodriguez <crodveg@yahoo.es>
2014-06-28 11:23:41 +0200

    Added problem 10994.

---
+++ UVaOnlineJudge/Problem_10994.fs
@@ -0,0 +1,16 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_10994 =
+  // Inputs
+  let p = 30
+  let q = 40
+
+  let rec F n =
+    match n with
+    | 0 -> 0
+    | _ when n % 10 > 0 -> n % 10
+    | _ -> F (n / 10)
+
+  let S p q = [p .. q] |> List.fold (fun acc elem -> acc + F elem) 0
+
+  printfn "%d" (S p q)
