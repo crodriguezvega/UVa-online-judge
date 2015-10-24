d310a91c1e582ff5a9cdddc63aaf8e09117de1d8
Carlos Rodriguez <crodveg@yahoo.es>
2015-10-24 09:51:28 +0200

    Update Problem_100.fs

--- UVaOnlineJudge/Problem_100.fs
+++ UVaOnlineJudge/Problem_100.fs
@@ -5,11 +5,11 @@ module Problem_100 =
   let i = 900
   let j = 1000
 
-  let rec cycleLength input =
-    match input with
+  let rec cycleLength n =
+    match n with
     | 1 -> 1
     | n when n % 2 = 0 -> 1 + cycleLength (n / 2)
-    | n -> 1 + cycleLength (3 * n + 1)
+    | _ -> 1 + cycleLength (3 * n + 1)
   let maximumCycleLength x y =
     [x..y] |> List.map cycleLength
            |> List.max
