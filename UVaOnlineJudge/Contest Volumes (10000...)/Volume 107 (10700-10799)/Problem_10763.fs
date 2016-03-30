f9ef0e298dd93e352a0aac719ba0df950eecbe22
Carlos Rodriguez <crodveg@yahoo.es>
2016-03-30 18:22:52 +0200

    Added solution to problem 10763.

---
+++ UVaOnlineJudge/Contest Volumes (10000...)/Volume 107 (10700-10799)/Problem_10763.fs
@@ -0,0 +1,26 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_10763 =
+  // Input
+  let exchange = [(1, 2);
+                  (2, 1);
+                  (3, 4);
+                  (4, 3);
+                  (100, 200);
+                  (200, 100);
+                  (57, 2);
+                  (2, 57);
+                  (1, 2);
+                  (2, 1)]
+
+  let isProgramValid exchange =
+    let rec loop exchange origin destination =
+      match exchange with
+      | [] -> (origin, destination)
+      | (o, d) :: tail -> loop tail (o :: origin) (d :: destination) 
+    let origin, destination = loop exchange List.empty List.empty
+    if (origin |> List.sort) = (destination |> List.sort) then "YES"
+    else "NO"
+
+  let result = isProgramValid exchange
+  printfn "%s" result
