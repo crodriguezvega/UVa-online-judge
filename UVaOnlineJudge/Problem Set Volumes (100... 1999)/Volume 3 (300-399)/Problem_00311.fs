c7ac6159b33cd063d281584fdd0cf8cd67a6d837
Carlos Rodriguez <crodveg@yahoo.es>
2016-04-02 08:40:44 +0200

    Added solution to problem 311.

---
+++ UVaOnlineJudge/Problem Set Volumes (100... 1999)/Volume 3 (300-399)/Problem_00311.fs
@@ -0,0 +1,12 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_311 =
+  // Input
+  let order = [7; 5; 1; 0; 0; 0]
+
+  let sizeParcel = 36.0;
+  let numberOfParcels = order |> List.mapi (fun i numberOfPackets -> numberOfPackets * pown (i + 1) 2)
+                              |> List.reduce (+)
+                              |> fun totalSize -> int (ceil (float totalSize) / sizeParcel)
+  
+  printfn "%d" numberOfParcels
