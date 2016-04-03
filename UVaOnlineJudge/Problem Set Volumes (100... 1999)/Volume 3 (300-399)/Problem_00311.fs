ac16be2fc6d469364db46d24659a7f2e6b5fda3b
Carlos Rodriguez <crodveg@yahoo.es>
2016-04-03 09:41:21 +0200

    Improved solution to problem 311.

--- UVaOnlineJudge/Problem Set Volumes (100... 1999)/Volume 3 (300-399)/Problem_00311.fs
+++ UVaOnlineJudge/Problem Set Volumes (100... 1999)/Volume 3 (300-399)/Problem_00311.fs
@@ -5,55 +5,36 @@ module Problem_311 =
   let order = [7; 5; 1; 0; 0; 0]
 
   let sizeParcel = 36;
-  let rec findNumberOfParcels order =
-    let rec loop packets size numberOfParcels =
+  let findNumberOfParcels order =
+    let rec loop packets size numberOfParcels (packetsOfSize1, packetsOfSize2) =
       match packets with
       | [] -> numberOfParcels
       | head :: tail ->
+        let newNumberOfParcels, (newPacketsOfSize1, newPacketsOfSize2) =
+          match size with
+          | 6 -> (numberOfParcels + head), (packetsOfSize1, packetsOfSize2)
+          | 5 -> (numberOfParcels + head), (packetsOfSize1 + head * 11, packetsOfSize2)
+          | 4 -> (numberOfParcels + head), (packetsOfSize1, packetsOfSize2 + head * 5)
+          | 3 ->
+            (numberOfParcels + (head + 3) / 4), match head % 4 with
+                                                | 1 -> (packetsOfSize1 + 7, packetsOfSize2 + 5)
+                                                | 2 -> (packetsOfSize1 + 6, packetsOfSize2 + 3)
+                                                | 3 -> (packetsOfSize1 + 5, packetsOfSize2 + 1)
+                                                | _ -> failwith "error"
+          | 2 ->
+            let newPacketsOfSize2 = (head - packetsOfSize2)
+            if newPacketsOfSize2 > 0 then
+              (numberOfParcels + (head + 8) / 9), (packetsOfSize1 + (sizeParcel - (newPacketsOfSize2 % 9) * 4), newPacketsOfSize2)
+            else
+              numberOfParcels, (packetsOfSize1 - newPacketsOfSize2 * 4, newPacketsOfSize2)
+          | 1 ->
+            let newPacketsOfSize1 = (head - packetsOfSize1)
+            if newPacketsOfSize1 > 0 then (numberOfParcels + (newPacketsOfSize1 + 35) / 36), (newPacketsOfSize1, packetsOfSize2)
+            else numberOfParcels, (newPacketsOfSize1, packetsOfSize2)
+          | _ -> failwith "error"
+
         let nextSize = size - 1
-        match size with
-        | _ when size = 6 -> 
-          loop tail nextSize (numberOfParcels + head)
-        | _ when size = 5 ->
-          let arr = tail |> List.rev |> Array.ofList
-          arr.[0] <- arr.[0] - head * 11
-          let lst = arr |> List.ofArray |> List.rev
-          loop lst nextSize (numberOfParcels + head)
-        | _ when size = 4 ->
-          let arr = tail |> List.rev |> Array.ofList
-          arr.[1] <- arr.[1] - head * 5
-          let lst = arr |> List.ofArray |> List.rev
-          loop lst nextSize (numberOfParcels + head)
-        | _ when size = 3 ->
-          let arr = tail |> List.rev |> Array.ofList
-
-          if (head % 4 = 1) then
-              arr.[1] <- arr.[1] - 5
-              arr.[0] <- arr.[0] - 7
-          else if (head % 4 = 2) then
-              arr.[1] <- arr.[1] - 3
-              arr.[0] <- arr.[0] - 6
-          else if (head % 4 = 3) then
-              arr.[1] <- arr.[1] - 1
-              arr.[0] <- arr.[0] - 5
-
-          let lst = arr |> List.ofArray |> List.rev
-          loop lst nextSize (numberOfParcels + (head + 3) / 4)
-        | _ when size = 2 ->
-          let arr = tail |> List.rev |> Array.ofList
-          if head > 0 then
-            arr.[0] <- arr.[0] - (sizeParcel - (head % 9) * 4)  
-            let lst = arr |> List.ofArray |> List.rev
-            loop lst nextSize (numberOfParcels + (head + 8) / 9)
-          else
-            arr.[0] <- arr.[0] + head * 4
-            let lst = arr |> List.ofArray |> List.rev
-            loop lst nextSize numberOfParcels
-        | _ when size = 1 ->
-          if head > 0 then loop tail nextSize (numberOfParcels + (head + 35) / 36)
-          else loop tail nextSize numberOfParcels
-        | _ -> failwith "error"
-
-    loop (List.rev order) 6 0
-
+        loop tail nextSize newNumberOfParcels (newPacketsOfSize1, newPacketsOfSize2)
+    loop (List.rev order) 6 0 (0, 0)
+  
   printfn "%d" (findNumberOfParcels order)
