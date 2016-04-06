56fbf1f30e12e5cfc94385c7aa9563c06b422342
Carlos Rodriguez <crodveg@yahoo.es>
2016-04-06 20:46:11 +0200

    Added solution to problem 10820.

---
+++ UVaOnlineJudge/Contest Volumes (10000...)/Volume 108 (10800-10899)/Problem_10820.fs
@@ -0,0 +1,22 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_10820 =
+  // Input
+  let N = 5
+
+  let rec sieve numbers =
+    match numbers with
+    | [] -> []
+    | (head :: tail) -> head :: sieve [for x in tail do if x % head > 0 then yield x]
+
+  let factorial number = [1..number] |> List.reduce (*)
+
+  let combinationsWithRepetitions chooseFrom chooseOf =
+    let a = [chooseFrom..chooseFrom + chooseOf - 1] |> List.reduce (*)
+    let b = [1..chooseOf] |> List.reduce (*)
+    a / b
+
+  let numberOfPrimes = sieve [2..N] |> List.length
+  let result = combinationsWithRepetitions numberOfPrimes 2
+
+  printfn "%d" result
