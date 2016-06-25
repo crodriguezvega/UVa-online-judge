dab6783971ada6f47582b71dea435fce538eb432
Carlos Rodriguez <crodveg@yahoo.es>
2016-06-25 20:18:17 +0200

    Added solution to problem 10012.

---
+++ UVaOnlineJudge/Contest Volumes (10000...)/Volume 100 (10000-10099)/Problem_10012.fs
@@ -0,0 +1,17 @@
+﻿namespace UVaOnlineJudge
+
+module Problem_10012 =
+  // Inputs
+  let radius = [2.0; 1.0; 4.0]
+
+  let calculateMinimumDistance (r1, r2) = sqrt (pown (r1 + r2) 2 - pown (r1 - r2) 2)
+
+  let length = radius.Length
+  let sumMinimumDistance = radius |> List.take (length - 1)
+                                  |> List.zip radius.Tail
+                                  |> List.map calculateMinimumDistance
+                                  |> List.sum
+  
+  let firstRadius = radius.Head
+  let lastRadius = radius |> List.skip (length - 1) |> List.head
+  printfn "%.3f" (firstRadius + sumMinimumDistance + lastRadius)
