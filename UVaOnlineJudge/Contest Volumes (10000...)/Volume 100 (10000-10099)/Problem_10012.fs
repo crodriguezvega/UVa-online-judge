10e6c871f846909e35aaf3ad8ebca800dc9f5b10
Carlos Rodriguez <crodveg@yahoo.es>
2016-06-25 20:19:01 +0200

    Updated solution to problem 10012.

--- UVaOnlineJudge/Contest Volumes (10000...)/Volume 100 (10000-10099)/Problem_10012.fs
+++ UVaOnlineJudge/Contest Volumes (10000...)/Volume 100 (10000-10099)/Problem_10012.fs
@@ -2,16 +2,16 @@
 
 module Problem_10012 =
   // Inputs
-  let radius = [2.0; 1.0; 4.0]
+  let radiuses = [2.0; 1.0; 4.0]
 
   let calculateMinimumDistance (r1, r2) = sqrt (pown (r1 + r2) 2 - pown (r1 - r2) 2)
 
-  let length = radius.Length
-  let sumMinimumDistance = radius |> List.take (length - 1)
-                                  |> List.zip radius.Tail
-                                  |> List.map calculateMinimumDistance
-                                  |> List.sum
+  let length = radiuses.Length
+  let sumMinimumDistance = radiuses |> List.take (length - 1)
+                                    |> List.zip radiuses.Tail
+                                    |> List.map calculateMinimumDistance
+                                    |> List.sum
   
-  let firstRadius = radius.Head
-  let lastRadius = radius |> List.skip (length - 1) |> List.head
+  let firstRadius = radiuses.Head
+  let lastRadius = radiuses |> List.skip (length - 1) |> List.head
   printfn "%.3f" (firstRadius + sumMinimumDistance + lastRadius)
