3dbccefdec216fb9c130b1a6ebf6528027cac9f9
Carlos Rodriguez <crodveg@yahoo.es>
2017-04-30 09:18:11 +0200

    Updated problem 10556.

--- UVaOnlineJudge/Contest Volumes (10000...)/Volume 105 (10500-10599)/Problem_10556.fsx
+++ UVaOnlineJudge/Contest Volumes (10000...)/Volume 105 (10500-10599)/Problem_10556.fsx
@@ -22,20 +22,21 @@
     let translation = { x = reference.x - point.x; y = reference.y - point.y }
     let doTranslate point = { x = point.x + translation.x; y = point.y + translation.y }
     polygon |> List.map doTranslate
+  
+  let check func polygon1 polygon2 =
+    let output = List.map2 func polygon1 polygon2 |> List.tail
+    let head = output |> List.head
+    output |> List.forall (fun x -> abs x - head < 0.00001)
 
   let isRotated polygon1 polygon2 =
     let angle p1 p2 = System.Math.Atan2(p1.y, p1.x) - System.Math.Atan2(p2.y, p2.x)
-    let angles = List.map2 angle polygon1 polygon2 |> List.tail
-    let head = angles |> List.head
-    angles |> List.forall (fun x -> abs x - head < 0.00001)
+    check angle polygon1 polygon2
 
   let isScaled polygon1 polygon2 =
     let norm p = sqrt (pown p.y 2 + pown p.x 2) 
     let scale p1 p2 = norm p1 / norm p2
-    let scales = List.map2 scale polygon1 polygon2 |> List.tail
-    let head = scales |> List.head
-    scales |> List.forall (fun x -> abs x - head < 0.00001)
-  
+    check scale polygon1 polygon2
+
   let isSimilar polygon1 polygon2 =
     isRotated polygon1 polygon2 && isScaled polygon1 polygon2
 
